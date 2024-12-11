509aa9ce30e613e870ee0bd370cd75c35dbc9ba1
timavik <103438439+timavik@users.noreply.github.com>
2024-12-11 17:13:52 +0300

    Add files via upload

---
+++ Lesson3DzOne/Lesson3DzOne/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Lesson3DzOne
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Введите свой возраст");
+      var a = Convert.ToInt32(Console.ReadLine());
+      var anwer = "Столько не живут :)";
+      if (a <= 2) { Console.WriteLine("Вы млоденец"); }
+      else if (a <= 12) { anwer = "Вы ребёнок"; }
+      else if (a <= 17) { anwer = "Вы подросток"; }
+      else if (a <= 59) { anwer = "Вы взрослый"; }
+      else if (a >= 60 && a <= 499) { anwer = "Вы пожилой"; }
+      
+      var cht = (a % 2 == 0) ? "чётный" : "нечётный";
+
+      Console.WriteLine($"Ваш возраст: {a}");
+      Console.WriteLine(anwer);
+      Console.WriteLine($"Ваш возраст является {cht} числом");
+    }
+  }
+}
---
+++ Lesson3DzTwo/Lesson3DzTwo/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Lesson3DzTwo
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.Write("Введите первое значение (true/false): ");
+      bool a = bool.Parse(Console.ReadLine());
+
+      Console.Write("Введите второе значение (true/false): ");
+      bool b = bool.Parse(Console.ReadLine());
+
+      bool andResult = a & b;
+      bool orResult = a | b;
+      bool xorResult = a ^ b;
+      bool andAndResult = a && b;
+      bool orOrResult = a || b;
+      bool notResultA = !a;
+      bool notResultB = !b;
+
+      Console.WriteLine("Результаты логических операций:");
+      Console.WriteLine("Операция|Результат");
+      Console.WriteLine("a & b    {0}\n", andResult);
+      Console.WriteLine("a | b    {0}\n", orResult);
+      Console.WriteLine("a ^ b    {0}\n", xorResult);
+      Console.WriteLine("a && b    {0}\n", andAndResult);
+      Console.WriteLine("a || b    {0}\n", orOrResult);
+      Console.WriteLine("!  a    {0}\n", notResultA);
+      Console.WriteLine("!  b    {0}\n", notResultB);
+    }
+  }
+}
